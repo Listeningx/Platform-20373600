8f6fbbec049dd3bb6f0e614052ed9ce7efe3312d
Shunning Liu <88953768+Listeningx@users.noreply.github.com>
2022-03-25 12:19:34 +0800

    lab2-homework-basic

---
+++ homework/附件/index.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+    if(Kernal.isLogin()) {
+        initUserInfo();
+    }
+
+    // 设置监听器，点击搜索按钮后，执行对应函数
+    document.getElementById('search-btn').addEventListener('click', function() {
+        var value = document.getElementById("textID").value ;
+        if(value == "") window.alert("请输入搜索内容");
+        else window.alert(value);
+        search();
+    });
+
+    // TODO: 在此为 top-right 元素设置监听器
+    document.getElementById('top-right').addEventListener('click',function(){
+        clickLogin();
+    })
+}
+
+function search() {
+    // TODO: 搜索触发后的行为
+    console.log('');
+    
+}
+
+function clickLogin() {
+    if(!Kernal.isLogin()) {
+        login();
+    }
+    else {
+        logout();
+    }
+}
+
+function initUserInfo() {
+    // TODO: 修改页面显示错误的 bug，另外注意图片路径是否正确
+    var username = Kernal.getUserName();
+    var content = '<div id="user" style="none">\
+                        <span id="user-img">\
+                            <img src="./img/user.jpg" alt=""/>\
+                        </span>\
+                        <span id="name">' + username + '</span>\
+                    </div>';
+    document.getElementById('top-right').innerHTML = content;
+}
+
+// ============================================================ 你不需要去关注的代码
+
+function login() {
+    Kernal.login();
+    location.reload();
+}
+
+function logout() {
+    Kernal.logout();
+    location.reload();
+}
